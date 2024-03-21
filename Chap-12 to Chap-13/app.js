58f96de7cfd8bc4d0de147995cbcb449541a2f14
rizwanqureshi-1 <https://github.com/rizwanqureshi-1/rizwanqureshi-1.git>
2024-03-21 19:28:42 +0500

    first commit

---
+++ Chap-12 to Chap-13/app.js
@@ -0,0 +1,40 @@
+//Chap-12 to 13
+
+// Q1
+//skip
+
+// Q2
+//skip
+
+// Q3
+// var user = +prompt("Enter number")
+// if ( user >= 1 ){
+//     alert( "Positive" + user >= 1 || user <= 10)
+// }else if( user <= -1 ){
+//     alert( "Negative" + user >= -1 || user <= -10)
+// }else if( user === 0 ){
+//     alert("Zero")
+// }
+
+
+// // Q4
+// var user = prompt("Enter vowel a,e,i,o,u")
+// if ( user === "a" || user === "e" || user === "i" || user === "o" || user === "u"){
+//     alert("true")
+// }else{
+//     alert("false")
+// }
+
+// Q5
+var pas = 123456789
+var user = +prompt("Enter pssword")
+if( user && "ifnot" ){
+    alert(" Please enter your password")
+}else if( user && pas ){
+    alert("Correct!")
+}else{
+    alert("Incorrect password")
+}
+
+
+
---
+++ Chap-17  to Chap-18/app.js
@@ -0,0 +1,68 @@
+// Chap 17 & 18
+
+// // Q1
+// var emptyArr = [" ", "", "", ""]
+// console.log(emptyArr)
+
+// // Q2
+// var numArr = [ "0", "1", "2", "3"]
+// document.write(numArr + "<br / >")
+// numArr.pop();
+// numArr.unshift("1")
+// document.write(numArr + "<br / >")
+// numArr.pop();
+// numArr.unshift("2")
+// document.write(numArr + "<br / >")
+
+
+// // Q3
+// var num = 1
+// for( a= 1;  a <= 10;  a++){
+//     document.write( a + "<br />")
+// }
+
+
+// // Q4
+// var num1 = +prompt("Enter number of table")
+// var num2 = +prompt("Enter how many time to multiply")
+// for( b=1 ; b <= num2; b++ ){
+//     document.write( num1 + " x" + b + " = " + b * 2 + "<br />")
+// }
+
+
+// // Q5  ==> sir nahi samjh aya ye wala
+// var fruit = ["apple", "banana", "mango", "orange", "strawberry"]
+// fruit = fruit.length
+// for (f = 0; f <= 5; f++) {
+//     document.write(fruit + "<br />")
+// }  
+
+
+// Q6
+
+// //a = counting
+// for( a=1; a<=10; a++){
+//     document.write(a  + "<br />")
+// // }
+
+// //b = Reverse counting
+// for( a=10; a>=1; a--){
+//     document.write( a + "<br/ >")
+// }
+
+// // c = Even number
+// for( a=0; a<=10; a++){
+//     document.write( a *2 + "<br />")
+// }
+
+// // d = Odd
+// var num = 2
+// for( a=1; a<=10; a++){
+//     document.write ( a*num-1 + "<br/>")
+// }
+
+// // e =
+// var num = 2
+// for(a=1; a<=10; a++){
+//     document.write(  a * num + "K" + "<br/>")
+// }
