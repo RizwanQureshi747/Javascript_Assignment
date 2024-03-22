2ee86f3ac812f8ec0656762bcbc16275f79bbe87
rizwanqureshi-1 <https://github.com/rizwanqureshi-1/rizwanqureshi-1.git>
2024-03-22 05:21:17 +0500

    first commit

--- Chap-12 to Chap-13/app.js
+++ Chap-12 to Chap-13/app.js
@@ -25,16 +25,39 @@
 //     alert("false")
 // }
 
-// Q5
-var pas = 123456789
-var user = +prompt("Enter pssword")
-if( user && "ifnot" ){
-    alert(" Please enter your password")
-}else if( user && pas ){
-    alert("Correct!")
-}else{
-    alert("Incorrect password")
-}
+// // Q5
+// var pas = 123456789
+// var user = prompt("Enter pssword")
+// if( user == "ifnot" ){
+//     alert(" Please enter your password")
+// }else if( user == pas ){
+//     alert("Correct!")
+// }else{
+//     alert("Incorrect password")
+// }
 
 
+// // Q6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//     alert("Good day");
+// }else{
+//     alert("Good evening");
+// }
 
+
+// // Q7
+// var time = +prompt("Enter time format like 1900=7pm;")
+// if( time >= "0000" && time < 1200){
+//     alert("Good Morning!")
+// }else if( time >= 1200 && time < 1700){
+//     alert("Good Afternoon!")
+// }else if( time >= 1700 && time < 2100){
+//     alert("Good Evening!")
+// }else if( time >= 2100 && time < 2400){
+//     alert("Good Night!")
+// }
+
+
+// END
---
+++ Chap-14 to Chap-16/app.js
@@ -0,0 +1,7 @@
+// chap-14 to 16
+
+// // Q1
+// var user = [];
+// console.log(user);
+
+// Q2
